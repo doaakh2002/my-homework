e6e07d75fa02615d3d37b50e69a0cf632106dea4
doaakh2002 <doaa07656@gmail.com>
2026-04-29 10:16:27 +0300

    Update hom.c

--- hom.c
+++ hom.c
@@ -1,1 +1,65 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
+#define SIZE 15
+
+typedef struct {
+    char buffer[SIZE];
+    int head;
+    int tail;
+    int count;
+} CircularBuffer;
+
+void init(CircularBuffer *cb) {
+    cb->head = 0;
+    cb->tail = 0;
+    cb->count = 0;
+}
+
+bool isFull(CircularBuffer *cb) {
+    return cb->count == SIZE;
+}
+
+bool isEmpty(CircularBuffer *cb) {
+    return cb->count == 0;
+}
+
+void write(CircularBuffer *cb, char data) {
+    if (isFull(cb)) {
+        printf("Buffer Overflow\n");
+        return;
+    }
+    cb->buffer[cb->head] = data;
+    cb->head = (cb->head + 1) % SIZE;
+    cb->count++;
+}
+
+char read(CircularBuffer *cb) {
+    if (isEmpty(cb)) {
+        printf("Buffer Underflow\n");
+        return '\0';
+    }
+    char data = cb->buffer[cb->tail];
+    cb->tail = (cb->tail + 1) % SIZE;
+    cb->count--;
+    return data;
+}
+
+int main() {
+    CircularBuffer cb;
+    init(&cb);
+    char name[20];
+    printf("Enter your name: ");
+    scanf("%s", name);
+    strcat(name, "CE-ESY");
+    for (int i = 0; i < strlen(name); i++) {
+        write(&cb, name[i]);
+    }
+    printf("Result: ");
+    while (!isEmpty(&cb)) {
+        printf("%c", read(&cb));
+    }
+    printf("\n");
+    return 0;
+}
